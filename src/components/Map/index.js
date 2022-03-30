ab77075135d2c98a31e923ddc37660628d5f921d
wakiem <joachimlefever@gmail.com>
2022-03-30 10:12:19 -0500

    Use IP geolocation and add loading spinner for map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import L from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
@@ -9,58 +9,69 @@ const MAP_TILE = L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
 });
 
 const MAP_PARAMS = {
-  zoom: 16,
+  zoom: 14,
   zoomControl: false,
   layers: [MAP_TILE]
 };
 
 const Map = (props) => {
 
+  const [isMapInitialized, setIsMapInitialized] = useState(false);
+
   const mapRef = useRef(null);
   const layerRef = useRef(null);
 
-  // Only executed on mount and unmount
+  // Initialization of the map, only executed on mount and unmount
   useEffect(() => {
-    // Set reference to map
-    mapRef.current = L.map('map', MAP_PARAMS);
-    // Ask user's position and center map on that position
-    navigator.geolocation.getCurrentPosition(function (position) {
-      mapRef.current.setView([position.coords.latitude, position.coords.longitude]);
-    });
-    // Add onClick handler to map
-    mapRef.current.on('click', (e) => {
-      props.addWaypoint(e.latlng.lat, e.latlng.lng);
-    });
-    // Add the layer on which we draw the waypoints and the route
-    layerRef.current = L.layerGroup().addTo(mapRef.current);
+    fetch("https://ipwhois.app/json/") // 10,000 free IP Geolocation requests per month
+      .then(res => res.json())
+      .then(
+        (result) => [result.latitude, result.longitude],
+        (error) => [51.0841312, 3.5741366]
+      )
+      .then((latlng) => {
+        // Init map and set reference
+        mapRef.current = L.map('map', { ...MAP_PARAMS, center: latlng });
+        // Add onClick handler to map
+        mapRef.current.on('click', (e) => {
+          props.addWaypoint(e.latlng.lat, e.latlng.lng);
+        });
+        // Add the layer on which we draw the waypoints and the route
+        layerRef.current = L.layerGroup().addTo(mapRef.current);
+        setIsMapInitialized(true);
+      })
   }, []);
 
-  // Executed on mount, unmount, and whenever the waypoints array changes
+  // Drawing the waypoints & route on the map, executed also whenever the waypoints array changes
   useEffect(() => {
-    // Clear the waypoints and the route from the map
-    layerRef.current.clearLayers()
-    // Loop over the waypoints to print them on the map, and to put together the route
-    const route = [];
-    props.waypoints.forEach((waypoint) => {
-      const markerIcon = L.divIcon({
-        html: `<span>${waypoint.id}</span>`,
-        className: 'waypoint-marker',
-        iconSize: [25, 25],
+    if (isMapInitialized) {
+      // Clear the waypoints and the route from the map
+      layerRef.current.clearLayers()
+      // Loop over the waypoints to print them on the map, and to put together the route
+      const route = [];
+      props.waypoints.forEach((waypoint) => {
+        const markerIcon = L.divIcon({
+          html: `<span>${waypoint.id}</span>`,
+          className: 'waypoint-marker',
+          iconSize: [25, 25],
+        });
+        const marker = L.marker([waypoint.lat, waypoint.lng], { icon: markerIcon });
+        marker.on('click', (e) => {
+          // Avoid double waypoints: Without this we could add the same (Lat, Lng) multiple times
+          L.DomEvent.stop(e);
+        })
+        marker.addTo(layerRef.current);
+        route.push([waypoint.lat, waypoint.lng]);
       });
-      const marker = L.marker([waypoint.lat, waypoint.lng], { icon: markerIcon });
-      marker.on('click', (e) => {
-        // Avoid double waypoints: Without this we could add the same (Lat, Lng) multiple times
-        L.DomEvent.stop(e);
-      })
-      marker.addTo(layerRef.current);
-      route.push([waypoint.lat, waypoint.lng]);
-    });
-    // Print the route on the map
-    L.polyline(route, { color: '#0E86E8', weight: 4 }).addTo(layerRef.current);
-  }, [props.waypoints]);
+      // Print the route on the map
+      L.polyline(route, { color: '#0E86E8', weight: 4 }).addTo(layerRef.current);
+    }
+  }, [isMapInitialized, props.waypoints]);
 
   return (
-    <div id="map" />
+    <div id="map">
+      {!isMapInitialized ? <div className="loader" /> : null}
+    </div>
   );
 
 }
